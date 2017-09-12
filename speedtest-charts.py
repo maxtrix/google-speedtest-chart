0f0036fe36127a05dc8a103d4f86e464edfbc08b
maxtrix <maxtrix@users.noreply.github.com>
2017-09-12 12:38:08 -0300

    Added option to specify server and format to Mbps
    -- Added a few lines of code to format the numbers from bps to Mbps (Based in the code of Grzegorz Bazior in https://stackoverflow.com/questions/12523586/python-format-size-application-converting-b-to-kb-mb-gb-tb)
    
    -- Added the line to specify the server to make the speedtest

--- speedtest-charts.py
+++ speedtest-charts.py
@@ -26,6 +26,37 @@ def submit_into_spreadsheet(download, upload, ping):
 
     sheet.append_table(values=data)
 
+def bits_2_human_readable(number_of_bits):
+	if number_of_bits < 0:
+		raise ValueError("!!! numberOfBits can't be smaller than 0 !!!")
+	
+	step_to_greater_unit = 1000.
+
+	number_of_bits = float(number_of_bits)
+	unit = 'bits'
+
+	if (number_of_bits / step_to_greater_unit) >= 1:
+		number_of_bits /= step_to_greater_unit
+		unit = 'Kb'
+
+	if (number_of_bits / step_to_greater_unit) >= 1:
+		number_of_bits /= step_to_greater_unit
+		unit = 'Mb'
+
+	if (number_of_bits / step_to_greater_unit) >= 1:
+		number_of_bits /= step_to_greater_unit
+		unit = 'Gb'
+
+	if (number_of_bits / step_to_greater_unit) >= 1:
+		number_of_bits /= step_to_greater_unit
+		unit = 'Tb'
+
+	precision = 1
+	number_of_bits = round(number_of_bits, precision)
+
+	return float(number_of_bits)
+
+
 def main():
     # Check for proper credentials
     print("Checking OAuth validity...")
@@ -33,10 +64,13 @@ def main():
 
     # Run speedtest and store output
     print("Starting speed test...")
+    #Use servers variable to specify the server to use with speedtest, servers listed in https://www.speedtestserver.com/ example servers=["ID_server"]
+    servers = []
     spdtest = speedtest.Speedtest()
+    spdtest.get_servers(servers)
     spdtest.get_best_server()
-    download = spdtest.download()
-    upload = spdtest.upload()
+    download = bits_2_human_readable(spdtest.download())
+    upload = bits_2_human_readable(spdtest.upload())
     ping = spdtest.results.ping
     print("Starting speed finished!")
 
